73a7c424bb9662b9ca5a686edf4279ff85e17af5
nikitaZeynalov <pk.ap.2003@inbox.ru>
2023-05-26 04:29:18 +0300

    even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 // eslint-disable-next-line import/no-unresolved
-import runEvenGame from '../src/games/even';
+import runEvenGame from '../src/games/even.js';
 
 runEvenGame();
