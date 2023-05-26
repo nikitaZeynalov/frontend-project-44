f7d5059a8fc44fd00d23e612e5b3f2d8454f73a2
nikitaZeynalov <pk.ap.2003@inbox.ru>
2023-05-26 04:58:50 +0300

    FixEslint

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/no-unresolved
+// eslint-disable-next-line import/no-unresolved, import/extensions
 import runEvenGame from '../src/games/even.js';
 
 runEvenGame();
