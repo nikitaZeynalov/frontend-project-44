c0dcd47527f47fc723e2b19fed0dbe18d6b35166
nikitaZeynalov <pk.ap.2003@inbox.ru>
2023-05-26 04:41:49 +0300

    FixBrainEven

---
+++ src/Games/runEvenGame.js
@@ -0,0 +1,13 @@
+import { getRandomNumber } from '../utils.js';
+import runGameLogic, { roundsNumber } from '../gameLogic.js';
+
+const runEvenGame = () => {
+  const rounds = [];
+  for (let i = 0; i < roundsNumber; i += 1) {
+    const question = getRandomNumber(1, 100);
+    const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+    rounds[i] = [question, correctAnswer];
+  }
+  return runGameLogic(rounds, 'Answer "yes" if the number is even, otherwise answer "no"');
+};
+export default runEvenGame;
